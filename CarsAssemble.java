7a9241959628336fd5cb0662728678456d629709
Alvaro Luque Moreno <luquemorenoalvaro@gmail.com>
2025-09-09 17:46:05 +0200

    Create CarsAssemble.java

---
+++ CarsAssemble.java
@@ -0,0 +1,23 @@
+package EXERCISM;
+
+public class CarsAssemble {
+    public static productionRatePerHour (int speed) {
+
+        int base = speed * 221;
+
+        if (speed >= 0 && speed <= 4)
+        return base;
+
+        if (speed >= 5 && speed <= 8)
+        return base * 0.9;
+
+        if (speed == 9)
+        return base * 0.8;
+
+        if (speed == 10)
+        return base * 0.77;
+
+        else 
+        return 0;
+    }
+}
