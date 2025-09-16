099c86e2d4f62771e8d285470be9f37fe3799352
Alvaro Luque Moreno <luquemorenoalvaro@gmail.com>
2025-09-16 13:28:52 +0200

    Update CarsAssemble.java

--- CarsAssemble.java
+++ CarsAssemble.java
@@ -1,27 +1,26 @@
 package EXERCISM;
 
 public class CarsAssemble {
-    public static productionRatePerHour (int speed) {
-
-        int base = speed * 221;
+    public static double productionRatePerHour(int speed) {
+        double base = speed * 221;
 
         if (speed >= 0 && speed <= 4)
-        return base;
+            return base;
 
         if (speed >= 5 && speed <= 8)
-        return base * 0.9;
+            return base * 0.9;
 
         if (speed == 9)
-        return base * 0.8;
+            return base * 0.8;
 
         if (speed == 10)
-        return base * 0.77;
+            return base * 0.77;
 
         else 
-        return 0;
+            return 0;
     }
 
-    public static workingItemsPerMinute (int speed) {
+    public static int workingItemsPerMinute(int speed) {
         return (int) (productionRatePerHour(speed) / 60);
     }
 }
